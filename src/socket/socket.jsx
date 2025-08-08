1a6781c09665ced4266f7fc1ab2dc3a5fa22c71d
peter Benjamin ani <benjaminparish6@gmail.com>
2025-08-08 15:34:35 +0100

    feat: deployinh a stable change

--- src/socket/socket.jsx
+++ src/socket/socket.jsx
@@ -1,4 +1,13 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:8080/");
+
+const socket = io("https://dex-naija-whot-backend.onrender.com", {
+  transports: ['polling', 'websocket'],
+  upgrade: true,
+  timeout: 20000,
+  forceNew: false,
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000
+});
 
 export default socket;
