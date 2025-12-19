740e809b372bebffb554d702617d637d8b80f494
Peter Benjamin Ani <benjaminparish6@gmail.com>
2025-12-19 18:05:11 +0100

    chore: Refine CopyLink UI, fix footer, and add advanced animations

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import style from "./index.module.css";
 
-function Footer() {
+function Footer({ className, style: customStyle }) {
   return (
-    <footer className={style.footer}>
+    <footer className={`${style.footer} ${className || ''}`} style={customStyle}>
       Built by{" "}
       <a href="https://celo.org" target={"_blank"} rel="noreferrer">
         Vortex-hue
