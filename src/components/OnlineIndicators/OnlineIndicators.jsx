641a6ad3d5a9646e259280442aa8809aacc51c34
Peter Benjamin Ani <benjaminparish6@gmail.com>
2025-12-21 14:42:19 +0100

    fix(stability): Shield OnlineIndicators against null prop

--- src/components/OnlineIndicators/OnlineIndicators.jsx
+++ src/components/OnlineIndicators/OnlineIndicators.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import OnlineIndicator from "../OnlineIndicator/OnlineIndicator";
 import style from "./index.module.css";
 
-function OnlineIndicators({ onlineState }) {
+function OnlineIndicators({ onlineState = { userIsOnline: false, opponentIsOnline: false } }) {
   return (
     <div className={style.indicators}>
       <OnlineIndicator online={onlineState.opponentIsOnline} />
