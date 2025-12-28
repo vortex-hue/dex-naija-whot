6774689132576ef9a106f9e1ef2db0063a7fd811
Peter Benjamin Ani <benjaminparish6@gmail.com>
2025-12-28 17:03:47 +0100

    chore: disable footer and resolve warnings

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,16 @@
-import React from "react";
-import style from "./index.module.css";
+// import React from "react";
+// import style from "./index.module.css";
 
-return (
-  <footer className={`${style.footer} ${className || ''}`} style={customStyle}>
-    Built by{" "}
-    <a href="https://celo.org" target={"_blank"} rel="noreferrer">
-      Vortex-hue
-    </a>
-  </footer>
-);
+function Footer() {
+  return null;
+  // return (
+  //   <footer className={`${style.footer} ${className || ''}`} style={customStyle}>
+  //     Built by{" "}
+  //     <a href="https://celo.org" target={"_blank"} rel="noreferrer">
+  //       Vortex-hue
+  //     </a>
+  //   </footer>
+  // );
 }
 
 export default Footer;
